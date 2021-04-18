dd08267b0e540462e40cf3d4e63069fd87b3f3d0
leafor <leafor.yzy1@outlook.com>
2021-04-18 14:52:41 +0800

    第一次获取到数据时退出程序

--- main.go
+++ main.go
@@ -24,6 +24,7 @@ type Task struct {
 const PORT = 10043
 
 func Handle(rw http.ResponseWriter, req *http.Request) {
+	defer os.Exit(0)
 	rw.WriteHeader(http.StatusOK)
 	decoder := json.NewDecoder(req.Body)
 	task := new(Task)
@@ -31,15 +32,14 @@ func Handle(rw http.ResponseWriter, req *http.Request) {
 		panic(err)
 	}
 	for i, x := range task.Tests {
-		os.WriteFile(fmt.Sprintf("in%d.txt", i+1), []byte(x.Input), 0600)
-		os.WriteFile(fmt.Sprintf("ans%d.txt", i+1), []byte(x.Output), 0600)
+		os.WriteFile(fmt.Sprintf("in%d.txt", i+1), []byte(x.Input), 0666)
+		os.WriteFile(fmt.Sprintf("ans%d.txt", i+1), []byte(x.Output), 0666)
 	}
-	println(fmt.Sprintf("parsed %d testcases form %d\n", len(task.Tests), PORT))
-	println("parsed from " + task.Contest)
+	fmt.Printf("parsed %d testcases form %d\n", len(task.Tests), PORT)
 }
 
 func main() {
-	println(fmt.Sprintf("listening form port %d\n", PORT))
+	fmt.Printf("listening form port %d\n", PORT)
 	http.HandleFunc("/", Handle)
 	http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", PORT), nil)
 }
